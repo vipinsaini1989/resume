c5781a42a674bdafd556ae7007a98633ce4447d6
vipinsaini1989 <vipinsaini1989@gmail.com>
2021-02-05 22:47:50 +0530

    Add print button on navbar

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,16 +1,21 @@
 <script>
     import Github from '../assets/github.svg';
-    import Linkedin from '../assets/linkedin.svg'
+    import Linkedin from '../assets/linkedin.svg';
+    import Printer from '../assets/printer.svg';
 </script>
 
 <nav class="navbar navbar-light bg-light d-print-none">
     <div class="container-fluid">
         <div class="d-none d-sm-block"></div>
         <div class="navbar-text social-btns-div">
-            <a href="https://github.com/vipinsaini1989" target="_blank" class="social-link">
+            <!-- svelte-ignore a11y-invalid-attribute -->
+            <a href="javascript:void(0);" on:click={()=> window.print()} title="print" role="button" class="social-link">
+                <Printer width="20"/>
+            </a>
+            <a href="https://github.com/vipinsaini1989" target="_blank" title="github" class="social-link">
                 <Github width="20"/>
             </a>
-            <a href="www.linkedin.com/in/vipin-saini-76a76765" target="_blank" class="social-link">
+            <a href="www.linkedin.com/in/vipin-saini-76a76765" target="_blank" title="linkedin" class="social-link">
                 <Linkedin width="20"/>
             </a>
         </div>
@@ -23,5 +28,10 @@
         padding: .6rem;
         border-radius: 50%;
         margin-left: .5rem;
+      }
+
+      .social-link:hover{
+        color: var(--bs-primary) !important;
+        border: 1px solid var(--bs-primary);
       }
   </style>
